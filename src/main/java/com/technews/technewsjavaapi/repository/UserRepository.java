e6c0ef370c7bae2d006ea65bbeb0dd2f5a3e9498
Christopher <ctijerina@live.com>
2022-05-12 15:59:27 -0500

    added repositories for User, Post, Vote, and Comment

---
+++ src/main/java/com/technews/technewsjavaapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.technews.technewsjavaapi.repository;
+
+
+import com.technews.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findUserByEmail(String email) throws Exception;
+
+}
+
